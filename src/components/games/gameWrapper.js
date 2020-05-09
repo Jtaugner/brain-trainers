ab494f4dac240f3eb9bfd62e501d6116b1767702
Kaplin Roman <31822824+Jtaugner@users.noreply.github.com>
2020-05-09 17:06:05 +0300

    change wrapper

--- src/components/games/gameWrapper.js
+++ src/components/games/gameWrapper.js
@@ -2,19 +2,24 @@ import {Component} from 'react';
 
 let interval;
 class GameWrapper extends Component{
-
     setTimer(){
         interval = setInterval(()=>{
+            if(this.props.gameDone){
+                clearInterval(interval);
+                return;
+            }
             if(this.state.timer - 1 < 1){
                 clearInterval(interval);
                 this.props.getLose('К сожалению, вы не уложились в нужное время');
-            }else{
-                this.setState((state) => {
-                    return {timer: state.timer - 1};
-                });
             }
+            this.setState((state) => {
+                return {timer: state.timer - 1};
+            });
         }, 1000)
 
+    }
+    clearTimer(){
+        clearInterval(interval)
     }
     componentWillUnmount() {
         clearInterval(interval);
