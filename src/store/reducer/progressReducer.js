096d7301f6a4a3cccb497a5f8d352136a5a7b7ea
Jtaugner <rak123rak123@mail.ru>
2020-06-07 20:52:35 +0300

    Change save data

--- src/store/reducer/progressReducer.js
+++ src/store/reducer/progressReducer.js
@@ -19,7 +19,7 @@ if(gamesProgress){
         gamesProgress[key] = newProgress;
     });
 }
-
+//{"chet":{"doneLevels":[0,0,0,0],"gameOpen":false,"openedLevels":[1,1,1,0]},"couple":{"doneLevels":[0,0,0,0],"gameOpen":false,"openedLevels":[1,1,1,0]},"field":{"doneLevels":[2,1,2,0],"gameOpen":true,"openedLevels":[1,1,1,0]},"findLetters":{"doneLevels":[0,0,0,0],"gameOpen":false,"openedLevels":[1,1,1,0]},"findWord":{"doneLevels":[3,6,2,0],"gameOpen":true,"openedLevels":[1,1,1,0]},"rememberNumbers":{"doneLevels":[5,2,1,0],"gameOpen":true,"openedLevels":[1,1,1,0]},"runWords":{"doneLevels":[0,0,0,0],"gameOpen":false,"openedLevels":[1,1,1,0]},"shultz":{"doneLevels":[15,7,9,0],"gameOpen":true,"openedLevels":[1,1,1,0]},"wordInText":{"doneLevels":[5,5,5,0],"gameOpen":true,"openedLevels":[1,1,1,0]}}
 export const progressReducer = (state = gamesProgress, action) => {
     if(action.type === ADD_DONE_LEVEL){
         const newState = {...state};
@@ -31,12 +31,12 @@ export const progressReducer = (state = gamesProgress, action) => {
     }else if(action.type === CHANGE_PROGRESS_FROM_PLAYER_DATA){
         return action.progress;
     }else if(action.type === BUY_GAME){
-        const newState = {...gamesProgress};
+        const newState = {...state};
         newState[action.payload.game].gameOpen = true;
         localStorage.setItem('gamesProgress', JSON.stringify(newState));
         return newState;
     }else if(action.type === BUY_LEVELS_DIFF){
-        const newState = {...gamesProgress};
+        const newState = {...state};
         newState[action.payload.game].openedLevels[action.payload.level] = 1;
         localStorage.setItem('gamesProgress', JSON.stringify(newState));
         return newState;
